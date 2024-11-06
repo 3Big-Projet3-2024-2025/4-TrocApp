42e9acd117978f85fc5a0d295694f2e571a4e8a3
Roxane Liébecq <127318216+la227938@users.noreply.github.com>
2024-11-06 12:13:48 +0100

    Création des dépôts
    Création du dépôt en Springboot pour la partie Backend (TrocAppBackend) et du dépôt en Angular pour la partie Frontend (TrocAppFrontend)

---
+++ TrocAppBackend/src/main/java/helha/trocappbackend/TrocAppBackendApplication.java
@@ -0,0 +1,13 @@
+package helha.trocappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrocAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrocAppBackendApplication.class, args);
+	}
+
+}
---
+++ TrocAppBackend/src/test/java/helha/trocappbackend/TrocAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package helha.trocappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrocAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
