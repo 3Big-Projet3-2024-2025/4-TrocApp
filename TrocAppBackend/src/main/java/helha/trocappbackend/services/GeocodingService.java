1019b5337f3e645d965a5e4ef4ccc823a4c6d7d2
Roxane Liébecq <127318216+la227938@users.noreply.github.com>
2024-11-18 12:14:57 +0100

    Création pour la localisation des objets pour la carte
    Création de la classe Location pour convertir l'adresse du propriétaire de l'objet en une localisation avec latitude et longitude ainsi qu'un service réalisant la transformation de l'adresse de l'objet en une localisation

---
+++ TrocAppBackend/src/main/java/helha/trocappbackend/services/GeocodingService.java
@@ -0,0 +1,41 @@
+package helha.trocappbackend.services;
+
+import helha.trocappbackend.models.Location;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class GeocodingService {
+
+    private final String GEOCODING_URL = "https://nominatim.openstreetmap.org/search?format=json&q=";
+    private final RestTemplate restTemplate;
+
+    public GeocodingService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Location getCoordinatesFromAddress(String address) {
+        // Construct the URL for the geocoding service
+        String url = GEOCODING_URL + address;
+
+        // Make the API request to get coordinates based on address
+        Location[] locations = restTemplate.getForObject(url, Location[].class);
+
+        if (locations != null && locations.length > 0) {
+            // Return the first location found
+            return new Location(locations[0].getLatitude(), locations[0].getLongitude());
+        }
+
+        return null;  // Return null if no location was found
+    }
+
+    // Service method to convert address to location and set it on the Object
+    public Object createObjectWithLocation(Object object) {
+        String address = object.getAddress();
+        Location location = getCoordinatesFromAddress(address);
+        if (location != null) {
+            object.setLocation(location);  // Set the location in the Object entity
+        }
+        return object;  // Return the object with the geocoded location
+    }
+}
