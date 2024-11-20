7d51342e5f3ca170aae4d8d184203bbaba926d97
Hayriye Dogan <la226220@student.helha.be>
2024-11-20 12:13:27 +0100

    Chagement de la class "Object" en "Item" et Modification de la class "User"

---
+++ TrocAppBackend/src/main/java/helha/trocappbackend/models/Item.java
@@ -0,0 +1,82 @@
+package helha.trocappbackend.models;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private String description;
+    private String photo;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+    private int ownerId;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User owner;
+
+    // Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
