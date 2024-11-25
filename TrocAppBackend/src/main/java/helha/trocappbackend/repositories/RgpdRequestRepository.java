a31cc6affd8b2157a9aed2033e09ea26c9f8e67d
la227391 <la227391@student.helha.be>
2024-11-25 12:14:42 +0100

    Implémentation des classes pour  RGPD
     Implémentation de la classe RgpdRequest , RgpdRequestService , RgpdRequestCategory , RgpdRequestRepository

---
+++ TrocAppBackend/src/main/java/helha/trocappbackend/repositories/RgpdRequestRepository.java
@@ -0,0 +1,7 @@
+package helha.trocappbackend.repositories;
+
+import helha.trocappbackend.models.RgpdRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class RgpdRequestRepository  {
+}
