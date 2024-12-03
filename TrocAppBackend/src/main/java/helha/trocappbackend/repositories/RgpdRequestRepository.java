7d9c6602404903f8478b0a5d1e882d435b59d34d
la227391 <la227391@student.helha.be>
2024-12-03 21:52:07 +0100

    Changement du nom des classes

--- TrocAppBackend/src/main/java/helha/trocappbackend/repositories/RgpdRequestRepository.java
+++
@@ -1,7 +0,0 @@
-package helha.trocappbackend.repositories;
-
-import helha.trocappbackend.models.RgpdRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public class RgpdRequestRepository  {
-}
