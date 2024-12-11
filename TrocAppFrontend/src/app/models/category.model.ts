c570758fe109b7b6e1aee3b32d0a21374ec3d4af
la227391 <la227391@student.helha.be>
2024-12-11 17:22:23 +0100

    Implementation of the category model

---
+++ TrocAppFrontend/src/app/models/category.model.ts
@@ -0,0 +1,9 @@
+export class Category {
+  id_category: number;
+  name: string;
+
+  constructor(id_category: number, name: string) {
+    this.id_category = id_category;
+    this.name = name;
+  }
+}
