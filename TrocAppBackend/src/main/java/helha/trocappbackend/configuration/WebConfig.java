6107b429de768874ea2f14b0e51bce17da4c1349
la227391 <la227391@student.helha.be>
2024-12-11 17:19:59 +0100

    Implementation of the category management components : category-list.component.css

---
+++ TrocAppBackend/src/main/java/helha/trocappbackend/configuration/WebConfig.java
@@ -0,0 +1,37 @@
+package helha.trocappbackend.configuration;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Applique CORS à toutes les requêtes (tous les endpoints)
+                .allowedOrigins("http://localhost:4200")  // Autorise les requêtes depuis Angular (localhost:4200)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Méthodes HTTP autorisées
+                .allowedHeaders("*")  // Autorise tous les en-têtes
+                .allowCredentials(true);  // Permet d'envoyer des informations d'authentification (cookies, tokens, etc.)
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        return mapper;
+    }
+}
