141b3afba1ac4c8bac166794ed19b26158252a8c
Hayriye Dogan <la226220@student.helha.be>
2024-12-14 19:45:53 +0100

    Finishing the Exchange class

--- TrocAppBackend/src/main/java/helha/trocappbackend/models/Exchange.java
+++ TrocAppBackend/src/main/java/helha/trocappbackend/models/Exchange.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Exchange {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private int id_exchange;
 
     private int offeredObjectId;
     private int requestedObjectId;
@@ -28,9 +28,84 @@ public class Exchange {
     @JoinColumn(name = "receiver_id")
     private User receiver;
 
-
-
     // Getters and Setters
+    public int getId_exchange() {
+        return id_exchange;
+    }
 
+    public void setId_exchange(int id_exchange) {
+        this.id_exchange = id_exchange;
+    }
 
+    public int getOfferedObjectId() {
+        return offeredObjectId;
+    }
+
+    public void setOfferedObjectId(int offeredObjectId) {
+        this.offeredObjectId = offeredObjectId;
+    }
+
+    public int getRequestedObjectId() {
+        return requestedObjectId;
+    }
+
+    public void setRequestedObjectId(int requestedObjectId) {
+        this.requestedObjectId = requestedObjectId;
+    }
+
+    public Date getProposalDate() {
+        return proposalDate;
+    }
+
+    public void setProposalDate(Date proposalDate) {
+        this.proposalDate = proposalDate;
+    }
+
+    public Date getAcceptanceDate() {
+        return acceptanceDate;
+    }
+
+    public void setAcceptanceDate(Date acceptanceDate) {
+        this.acceptanceDate = acceptanceDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public float getFirstEvaluation() {
+        return firstEvaluation;
+    }
+
+    public void setFirstEvaluation(float firstEvaluation) {
+        this.firstEvaluation = firstEvaluation;
+    }
+
+    public float getSecondEvaluation() {
+        return secondEvaluation;
+    }
+
+    public void setSecondEvaluation(float secondEvaluation) {
+        this.secondEvaluation = secondEvaluation;
+    }
+
+    public User getInitiator() {
+        return initiator;
+    }
+
+    public void setInitiator(User initiator) {
+        this.initiator = initiator;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
 }
