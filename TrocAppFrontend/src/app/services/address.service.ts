cf318b4cfee425ff902cdf68a8645a14377190c3
Roxane Liébecq <127318216+la227938@users.noreply.github.com>
2024-12-16 12:13:28 +0100

    Adding the Address service (Frontend)
    Adding the component for the Address Service for the frontend part

---
+++ TrocAppFrontend/src/app/services/address.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+
+  private baseUrl = 'http://localhost:8080/address';
+
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private httpClient : HttpClient) { }
+
+    getAddressById(id: number): Observable<any> {
+      return this.httpClient.get(`${this.baseUrl}/${id}`).pipe(
+        catchError((error) => {
+          console.error(`Error fetching address with ID ${id}:`, error);
+          return throwError(() => new Error(`Error fetching address with ID ${id}.`));
+        })
+      );
+    }
+
+}
