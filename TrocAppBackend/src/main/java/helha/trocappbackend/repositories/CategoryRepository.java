d351a04dba26af4413bcd18a9cea59fdc6a72fb4
la227391 <la227391@student.helha.be>
2024-12-17 15:09:20 +0100

    Update of the class CatgoryRepository

--- TrocAppBackend/src/main/java/helha/trocappbackend/repositories/CategoryRepository.java
+++ TrocAppBackend/src/main/java/helha/trocappbackend/repositories/CategoryRepository.java
@@ -1,8 +1,12 @@
 package helha.trocappbackend.repositories;
 
 import helha.trocappbackend.models.Category;
+import helha.trocappbackend.models.GdprRequest;
+import helha.trocappbackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
