32231a09f3b8b771da33f9364baad21a76a42ece
Roxane Liébecq <127318216+la227938@users.noreply.github.com>
2024-12-17 20:26:48 +0100

    Update of the routes (Frontend)
    Update of the routes with the map for the home page and the routes to the others components

--- TrocAppFrontend/src/app/app.routes.ts
+++ TrocAppFrontend/src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { UsersManagementComponent } from './users-management/users-management.component';
 import { ViewingItemsMapComponent } from './viewing-items-map/viewing-items-map.component';
 import { DetailedViewItemComponent } from './detailed-view-item/detailed-view-item.component';
-export const routes: Routes = [
 
+
+export const routes: Routes = [
+    { path: '', component: ViewingItemsMapComponent }, // Default Path (Home Page)
     //Path for user management as admin
     { path: "admin/users-management", component: UsersManagementComponent, /*canActivate: [authGuard]*/ },
     //Path for viewing items on a map
-    { path: "/viewing-items-map", component: ViewingItemsMapComponent },
+    { path: "viewing-items-map", component: ViewingItemsMapComponent }, // Path to the map page
     //Path for viewing a detailed item
-    { path: "/detailed-view-item", component: DetailedViewItemComponent }
+    { path: "detailed-view-item", component: DetailedViewItemComponent }, // Path to the detailed item page
+    { path: '**', redirectTo: 'viewing-items-map', pathMatch: 'full' } // Wildcard route
 ];
