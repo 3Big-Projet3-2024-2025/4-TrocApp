7f372404098b647cd1d64805783aac1d3a460750
la227391 <la227391@student.helha.be>
2024-12-17 15:08:50 +0100

    Update of the class Category

---
+++ TrocAppBackend/src/main/java/helha/trocappbackend/repositories/GdprRequestRepository.java
@@ -0,0 +1,13 @@
+package helha.trocappbackend.repositories;
+
+import helha.trocappbackend.models.GdprRequest;
+import helha.trocappbackend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GdprRequestRepository extends JpaRepository<GdprRequest, Integer>  {
+    List<GdprRequest> findByUser(User user);
+
+    List<GdprRequest> findByAdminUser(User adminUser);
+}
