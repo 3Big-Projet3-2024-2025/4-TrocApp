808eb966e75f2b767538600822ee0bc661ebc5c3
Hayriye Dogan <la226220@student.helha.be>
2025-01-02 16:14:40 +0100

    Creation of EmailService for sending emails to users

---
+++ TrocAppBackend/src/main/java/helha/trocappbackend/services/EmailService.java
@@ -0,0 +1,36 @@
+package helha.trocappbackend.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public EmailService() {
+        super();
+    }
+
+    public void sendEmail(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            String smtpUser = "la226220@student.helha.be";
+            message.setFrom(smtpUser);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+
+            mailSender.send(message);
+
+            System.out.println("Email succesfully sent");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Sending an email failed " + e.getMessage());
+        }
+    }
+
+}
