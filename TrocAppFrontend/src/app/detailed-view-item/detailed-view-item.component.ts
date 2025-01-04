86d8752f648d0016800cb5f4519f47f56b31e8b7
Roxane Liébecq <127318216+la227938@users.noreply.github.com>
2025-01-04 23:24:50 +0100

    Update detailed view component
    Added retrieval of the category object via its id obtained from the item to be displayed (TS + HTML)

--- TrocAppFrontend/src/app/detailed-view-item/detailed-view-item.component.ts
+++ TrocAppFrontend/src/app/detailed-view-item/detailed-view-item.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ItemService } from '../services/item.service';
 import { UsersService } from '../services/users.service';
+import { CategoryService } from '../services/category.service';
 import { Item } from '../models/item';
 import { User } from '../models/user';
+import { Category } from '../models/category.model';
 
 @Component({
   selector: 'app-detailed-view-item',
@@ -18,20 +20,30 @@ export class DetailedViewItemComponent {
 
   item!: Item; // Property for the item
   owner!: User; // Property for the owner of the item
+  category!: Category; // Property for the category of the item
 
   // Initialization 
   ngOnInit(): void {
     this.loadDataItem();
   } 
 
-  constructor( private itemService: ItemService, private usersService : UsersService ) { }
+  constructor( private itemService: ItemService, private usersService : UsersService, private categoryService : CategoryService ) { }
 
   // Load the data of the selected item
   loadDataItem() {
     this.itemService.getItem(this.id_selected_item).subscribe({
       next: (data) => {
-        this.item = data;
-        
+        this.item = data as Item;
+                
+        this.categoryService.getCategoryById(this.item.category.id_category).subscribe({
+          next: (data: any) => {
+            this.category = data;
+          },
+          error: (error: any) => {
+            console.error('Error while trying to fetch the category of the item :', error);
+          }
+        });
+
         this.usersService.getUserById(this.item.owner.id).subscribe({
           next: (data: any) => {
             this.owner = data;
