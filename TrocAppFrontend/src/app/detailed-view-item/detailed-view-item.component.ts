29ec59b46fc04cf2a19a0bf7805833b385a7f72c
Roxane Liébecq <127318216+la227938@users.noreply.github.com>
2025-01-04 18:09:05 +0100

    Update detailed view item component
    User service added to retrieve owner information, html/css changed

--- TrocAppFrontend/src/app/detailed-view-item/detailed-view-item.component.ts
+++ TrocAppFrontend/src/app/detailed-view-item/detailed-view-item.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ItemService } from '../services/item.service';
+import { UsersService } from '../services/users.service';
+import { Item } from '../models/item';
+import { User } from '../models/user';
 
 @Component({
   selector: 'app-detailed-view-item',
@@ -13,32 +16,39 @@ export class DetailedViewItemComponent {
 
   id_selected_item!: number; // Property for the selected item ID
 
-  item: any; // Property for the item
+  item!: Item; // Property for the item
+  owner!: User; // Property for the owner of the item
 
   // Initialization 
   ngOnInit(): void {
     this.loadDataItem();
   } 
 
-  successMessage: string = ''; // Property for the success message
-  errorMessage: string = ''; // Property for the error message
-  
-  constructor( private itemService: ItemService ) { }
+  constructor( private itemService: ItemService, private usersService : UsersService ) { }
 
   // Load the data of the selected item
   loadDataItem() {
-    this.itemService.getItem(this.id_selected_item).subscribe(
-      data => {
+    this.itemService.getItem(this.id_selected_item).subscribe({
+      next: (data) => {
         this.item = data;
+        
+        this.usersService.getUserById(this.item.owner.id).subscribe({
+          next: (data: any) => {
+            this.owner = data;
+          },
+          error: (error: any) => {
+            console.error('Error while trying to fetch the owner of the item :', error);
+          }
+        });
       },
-      error => {
-        this.errorMessage = 'Error while trying to fetch the item by ID';
-        console.log(error);
+      error: (error) => {
+        console.error("Error while trying to fetch the data of the item :" + error);
       }
-    );
+    });
   }
 
-  // Function to call when an user would like to do an exchange
-    proposeExchange(item: any) { }
-
+  // Function to call when a user would like to do an exchange
+  proposeExchange(id: number) {
+    
+  }
 }
