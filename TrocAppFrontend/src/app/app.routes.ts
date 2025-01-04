77174835828b010cd779d6c39b2aad77b4b9c168
la227391 <la227391@student.helha.be>
2025-01-04 23:43:24 +0100

    Update the app routes to add paths

--- TrocAppFrontend/src/app/app.routes.ts
+++ TrocAppFrontend/src/app/app.routes.ts
@@ -12,7 +12,8 @@ import { ItemFormComponent } from './item-form/item-form.component';
 //import { ItemDetTestComponent } from './item-det-test/item-det-test.component';
 //import { ExchangeListComponent } from './exchange-list/exchange-list.component';
 import { CategoryListComponent } from './category-list/category-list.component';
-
+import { GdprFormComponent } from './gdpr-form/gdpr-form.component';
+import { GdprAdminComponent } from './gdpr-admin/gdpr-admin.component';
 export const routes: Routes = [
     { path: '', component: ViewingItemsMapComponent }, // Default Path (Home Page)
     //Path for user management as admin
@@ -23,32 +24,33 @@ export const routes: Routes = [
     { path: "detailed-view-item", component: DetailedViewItemComponent },
     //{ path: 'edit/:id', component: UserEditComponent, /*canActivate: [authGuard]*/ },
     {path: 'category-list', component: CategoryListComponent},
-
-    { path: '**', redirectTo: '/category-list', pathMatch: 'full' },
-
     //{ path: '**', redirectTo: '/category-list', pathMatch: 'full' },
-
-
+    
     { path: "detailed-view-item/:id", component: DetailedViewItemComponent },
     //path for adding an item
     { path: "item", component: ItemFormComponent },
     //path for updating an item
     { path: "item/:id", component: ItemFormComponent },
-    //path by default
+    //path by default 
     { path: "", redirectTo: "/viewing-items-map", pathMatch: "full" },
 
     // { path: "itemDetTest/:id", component: ItemDetTestComponent },
     // {path: "exchanges", component: ExchangeListComponent },
     // { path: "itemtest", component: ItemtestComponent }
-
+  
     // Path for editing a user
     { path: 'edit/:id', component: UserEditComponent, /*canActivate: [authGuard]*/ },
 
-
+    
     { path: "users-management", component: UsersManagementComponent, /*canActivate: [authGuard]*/ },
     { path: 'edit/:id', component: UserEditComponent, /*canActivate: [authGuard]*/ },
     { path: 'edit-profil', component: EditProfilComponent, /*canActivate: [authGuard]*/ },
     { path: 'rating-comment', component: RatingCommentComponent, /*canActivate: [authGuard]*/ },
-    { path: '**', redirectTo: 'viewing-items-map', pathMatch: 'full' }, // Wildcard route
+    //{ path: '**', redirectTo: 'viewing-items-map', pathMatch: 'full' }, // Wildcard route
 
+    // Path for the GDPR form
+    {path : 'gdpr-form', component: GdprFormComponent},
+    // Path for the GDPR admin page
+    {path : 'gdpr-admin', component: GdprAdminComponent}
+    
 ];
