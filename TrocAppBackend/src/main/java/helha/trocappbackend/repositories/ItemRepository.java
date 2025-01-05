102f542bb40413d4836e18e8df1128107b205f84
Hayriye Dogan <la226220@student.helha.be>
2025-01-05 21:35:52 +0100

    Update of the interface ItemRepository, added javadoc

--- TrocAppBackend/src/main/java/helha/trocappbackend/repositories/ItemRepository.java
+++ TrocAppBackend/src/main/java/helha/trocappbackend/repositories/ItemRepository.java
@@ -3,6 +3,14 @@ package helha.trocappbackend.repositories;
 import helha.trocappbackend.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing Item entities.
+ *
+ * <p>This interface extends JpaRepository, providing standard CRUD operations
+ * and additional query methods for the Item entity.</p>
+ *  @author Hayriye Dogan
+ *  @see helha.trocappbackend.repositories
+ */
 public interface ItemRepository extends JpaRepository<Item, Integer> {
 
 }
