ce5271357485467d15b55fcf94fc4cdcc4f47201
la227391 <la227391@student.helha.be>
2025-01-05 15:15:54 +0100

    implementation of javadoc in GDPRRequestRepository

--- TrocAppBackend/src/main/java/helha/trocappbackend/repositories/GdprRequestRepository.java
+++ TrocAppBackend/src/main/java/helha/trocappbackend/repositories/GdprRequestRepository.java
@@ -6,7 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface GdprRequestRepository extends JpaRepository<GdprRequest, Integer>  {
-    List<GdprRequest> findByUser(User user);
+/**
+ * Repository interface for performing CRUD operations on {@link GdprRequest} entities.
+ * This interface extends {@link JpaRepository} to provide the standard persistence operations.
+ */
+public interface GdprRequestRepository extends JpaRepository<GdprRequest, Integer> {
 
+    /**
+     * Finds all GDPR requests associated with a specific user.
+     *
+     * @param user The user whose GDPR requests are to be retrieved.
+     * @return A list of {@link GdprRequest} entities associated with the given user.
+     */
+    List<GdprRequest> findByUser(User user);
 }
