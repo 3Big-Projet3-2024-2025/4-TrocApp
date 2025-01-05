b7a3dbeb0374118fdb216791bffd54707c06a59b
Roxane Liébecq <127318216+la227938@users.noreply.github.com>
2025-01-05 16:07:11 +0100

    Update of Address service
    Add of the headers for the token

--- TrocAppFrontend/src/app/services/address.service.ts
+++ TrocAppFrontend/src/app/services/address.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, Observable, throwError } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,17 @@ export class AddressService {
 
   private baseUrl = 'http://localhost:8080/addresses';
 
-  private httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-  };
+  constructor(private httpClient : HttpClient, private cookieService: CookieService) { }
 
-  constructor(private httpClient : HttpClient) { }
-
+  private getAuthHeaders(): HttpHeaders {
+    const token = this.cookieService.get('token');
+    return new HttpHeaders({
+      Authorization: 'Bearer ' + token
+    });
+  }
     getAddressById(id: number): Observable<any> {
-      return this.httpClient.get(`${this.baseUrl}/${id}`).pipe(
+      const headers = this.getAuthHeaders();
+      return this.httpClient.get(`${this.baseUrl}/${id}`, { headers } ).pipe(
         catchError((error) => {
           console.error(`Error fetching address with ID ${id}:`, error);
           return throwError(() => new Error(`Error fetching address with ID ${id}.`));
