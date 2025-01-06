88a2ee3a025d524aef816db4acba3ca5e6a8dbba
Hayriye Dogan <la226220@student.helha.be>
2025-01-06 04:35:02 +0100

    update item.service, added token

--- TrocAppFrontend/src/app/services/item.service.ts
+++ TrocAppFrontend/src/app/services/item.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Item } from '../models/item';
+import { CookieService } from 'ngx-cookie-service';
 
 
 @Injectable({
@@ -9,35 +10,68 @@ import { Item } from '../models/item';
 })
 export class ItemService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookieService: CookieService) { }
 
   backendURL = "http://localhost:8080/items";
 
   getAvailableItem(): Observable<Item[]>{
-      return this.http.get<Item[]>(this.backendURL+"/available");
+    const token = this.cookieService.get("token");
+      return this.http.get<Item[]>(this.backendURL+"/available", { headers: {
+        "Authorization" : "Bearer "+token
+        }
+      }
+    );
   }
 
   getItem(id?: number) : Observable<Item[] | Item>{
+    const token = this.cookieService.get("token");
     if(id) {
-      return this.http.get<Item>(this.backendURL+"/"+id);
+      return this.http.get<Item>(this.backendURL+"/"+id, { headers: {
+        "Authorization" : "Bearer "+token
+        }
+      }
+    );
     } else {
-      return this.http.get<Item[]>(this.backendURL);
+      return this.http.get<Item[]>(this.backendURL, { headers: {
+        "Authorization" : "Bearer "+token
+        }
+      }
+    );
     }
   }
 
   getItemByUserId(id: number): Observable<Item[]> {
-    return this.http.get<Item[]>(this.backendURL+"/user/"+id);
+    const token = this.cookieService.get("token");
+    return this.http.get<Item[]>(this.backendURL+"/user/"+id, { headers: {
+      "Authorization" : "Bearer "+token
+      }
+    }
+  );
   }
 
   saveItem(item: Item): Observable<Item> {
+    const token = this.cookieService.get("token");
     if(item.id>0) {
-      return this.http.put<Item>(this.backendURL+"/"+item.id, item);
+      return this.http.put<Item>(this.backendURL+"/"+item.id, item, { headers: {
+        "Authorization" : "Bearer "+token
+        }
+      }
+    );
     } else {
-      return this.http.post<Item>(this.backendURL, item);
+      return this.http.post<Item>(this.backendURL, item, { headers: {
+        "Authorization" : "Bearer "+token
+        }
+      }
+    );
     }
   }
 
   deleteItem(id: number): Observable<any> {
-    return this.http.delete(this.backendURL+"/"+id);
+    const token = this.cookieService.get("token");
+    return this.http.delete(this.backendURL+"/"+id, { headers: {
+      "Authorization" : "Bearer "+token
+      }
+    }
+  );
   }
 }
